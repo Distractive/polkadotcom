a2f1820dad26096e722a5e1c5dccfa8d011f190b
adrian-at-stink <107425773+adrian-at-stink@users.noreply.github.com>
2024-06-06 22:09:28 +0100

    chore: update small card layout (#113)

--- apps/web/features/page/blocks/cards-small/cards-small.tsx
+++ apps/web/features/page/blocks/cards-small/cards-small.tsx
@@ -12,24 +12,28 @@ interface Props {
 export function CardsSmallBlock({ cards }: Props) {
   return (
     <div key={cards._key} className="grid-system !mx-0 !px-0">
-      <div className={cn("col-span-12 pb-gutter lg:col-span-4")}>
+      <div className={cn("col-span-12 pb-gutter")}>
         <div className="flex flex-col gap-copy lg:w-5/6">
           <Heading variant="h2">{cards.heading}</Heading>
           {cards.body && <p>{cards.body}</p>}
         </div>
       </div>
-      <div
-        className={cn(
-          "grid-system col-span-12 !mx-0 gap-card !px-0 lg:col-span-12 lg:col-start-5"
-        )}
-      >
-        {cards.items.map((card) => (
-          <CardSmallBlock
-            key={card._key}
-            card={card}
-            className={cn("col-span-12")}
-          />
-        ))}
+      <div className={cn("grid-system col-span-12 !mx-0 gap-card !px-0")}>
+        {cards.items.map((card, index) => {
+          // span the last two cards if there are 2 cards in a row
+          const shouldSpan =
+            cards.items.length % 3 === 2 && index >= cards.items.length - 2
+          return (
+            <CardSmallBlock
+              key={card._key}
+              card={card}
+              className={cn(
+                "col-span-12 md:col-span-3 lg:col-span-4",
+                shouldSpan && "lg:col-span-6"
+              )}
+            />
+          )
+        })}
       </div>
     </div>
   )
