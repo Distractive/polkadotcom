eea6a11b23b74db3c940f416f34acbc8cbce044b
adrian-at-stink <107425773+adrian-at-stink@users.noreply.github.com>
2024-06-06 15:40:22 +0100

    chore: update unbounded font weight (#114)

--- packages/ui/components/ui/heading.tsx
+++ packages/ui/components/ui/heading.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "../../lib/utils"
 
 const HeadingStyles = {
-  base: "font-light font-display",
+  base: "font-display",
   variants: {
     h1: "text-6xl md:text-7xl leading-snug",
     h2: "text-4xl md:text-5xl leading-snug",
