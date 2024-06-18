63d6d10ad57cc2853005bf783f76627a5434fd0c
Matt Yates <matt.yates52@gmail.com>
2024-06-18 14:23:41 +0100

    fix: Cards logo spacing (#186)

--- apps/web/features/page/blocks/cards-logo/cards-logo.tsx
+++ apps/web/features/page/blocks/cards-logo/cards-logo.tsx
@@ -11,14 +11,12 @@ interface Props {
 
 export function CardsLogoBlock({ cards }: Props) {
   return (
-    <div key={cards._key} className="grid-system px-gutter">
-      <div className={cn("col-span-12 pb-gutter")}>
-        <div className="flex flex-col gap-copy lg:w-5/6">
-          <Heading variant="h2">{cards.heading}</Heading>
-          {cards.body && <p>{cards.body}</p>}
-        </div>
+    <div key={cards._key} className="grid-system gap-y-gutter px-gutter">
+      <div className="col-span-full flex flex-col gap-copy">
+        <Heading variant="h2">{cards.heading}</Heading>
+        {cards.body && <p>{cards.body}</p>}
       </div>
-      <div className={cn("grid-system col-span-12")}>
+      <div className={cn("grid-system col-span-full gap-gutter")}>
         {cards.items.map((card) => (
           <CardLogoBlock
             key={card._key}
