533b41f71178295594f30d1930422adf6998b610
Sophie Li Henry <sophiesophiehenry@googlemail.com>
2024-06-19 17:03:07 +0100

    Feat/button component (#205)

---
+++ apps/web/components/decorative-line.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@shared/ui"
+
+interface Props {
+  className?: string
+  children?: React.ReactNode
+  forButtonBlock?: boolean
+}
+
+export function DecorativeLine({
+  className,
+  children,
+  forButtonBlock = false,
+}: Props) {
+  return (
+    <div
+      className={cn(
+        "relative col-span-12 inline-flex items-center justify-center pt-gutter",
+        className
+      )}
+    >
+      <hr
+        className={cn(
+          "w-full border-grey-300",
+          forButtonBlock && "sm:hidden md:block"
+        )}
+      />
+      <div
+        className={cn(
+          "absolute bg-white",
+          forButtonBlock ? "w-full px-gutter md:w-auto" : "px-6"
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
---
+++ apps/web/stories/atoms/ButtonBlock.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { ButtonBlock } from "@/features/page/blocks/button-block"
+
+const meta = {
+  title: "Atoms/ButtonBlock",
+  component: ButtonBlock,
+  argTypes: {},
+} satisfies Meta<typeof ButtonBlock>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const PrimaryButtonBlock: Story = {
+  args: {
+    buttonBlock: {
+      _key: "buttonBlock",
+      link: {
+        internal: { slug: "learn-more" },
+        label: "Learn more",
+        variant: "primary",
+      },
+    },
+  },
+}
+
+export const SecondaryButtonBlock: Story = {
+  args: {
+    buttonBlock: {
+      _key: "buttonBlocktwo",
+      link: {
+        internal: null,
+        label: "Learn more",
+        variant: "secondary",
+        external: "https://www.example.com",
+      },
+    },
+  },
+}
