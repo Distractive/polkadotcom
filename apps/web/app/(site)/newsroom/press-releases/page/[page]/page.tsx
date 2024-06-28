0eee87cc1bd200bc81fbb7d5695e0ca7fa46f942
Adrian Friend <adrian@stinkstudios.com>
2024-06-28 12:31:58 +0100

    chore: test with just one static page

--- apps/web/app/(site)/newsroom/press-releases/page/[page]/page.tsx
+++ apps/web/app/(site)/newsroom/press-releases/page/[page]/page.tsx
@@ -2,6 +2,11 @@ import { PRESS_RELEASE_POSTTYPE } from "@/constants/global"
 import Layout from "@/features/posts/layout"
 
 export const dynamic = "force-static"
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  return [{ page: "1" }]
+}
 
 export default async function Page({
   params: { page },
