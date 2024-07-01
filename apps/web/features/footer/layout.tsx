d9c75b218012068eeea3be24cac3dc246717aaa7
Sophie Li Henry <sophiesophiehenry@googlemail.com>
2024-07-01 09:59:36 +0100

    fix: using jpg and picture element for better performance (#306)

--- apps/web/features/footer/layout.tsx
+++ apps/web/features/footer/layout.tsx
@@ -15,31 +15,54 @@ export default function FooterLayout({ footer }: Props) {
   return (
     <div
       className={cn(
-        "relative mt-page flex flex-col pt-page md:h-[70rem] md:min-h-[52rem] md:justify-end md:overflow-y-hidden lg:h-full"
+        "relative mt-page flex flex-col md:h-[70rem] md:justify-end md:overflow-y-hidden lg:h-full lg:min-h-[55rem]"
       )}
     >
-      <img
-        src="/footer/desk-footer-asset-left.webp"
-        alt=""
-        className="lg:height-[44rem] hidden md:absolute  md:left-0 md:top-0 md:block md:h-full md:max-w-[45rem] md:object-cover"
-      />
-      <img
-        src="/footer/desk-footer-asset-right.webp"
-        alt=""
-        className="lg:height-[44rem] hidden md:absolute md:bottom-0 md:right-0 md:block md:h-full md:w-full md:max-w-[46rem] md:object-cover lg:-bottom-[1rem]"
-      />
+      <picture>
+        <source
+          media="(min-width: 22.5rem)"
+          srcSet="/footer/desk-footer-asset-left.jpg"
+          className="lg:height-[44rem] hidden w-1/2 md:absolute md:left-0 md:top-[2rem] md:block md:h-full md:max-w-[42rem] md:object-cover md:object-[-20rem]"
+        />
+        <img
+          src="/footer/desk-footer-asset-left.jpg"
+          alt=""
+          loading="lazy"
+          className="lg:height-[44rem] hidden w-1/2 md:absolute md:left-0 md:top-[2rem] md:block md:h-full md:max-w-[42rem] md:object-cover md:object-[-20rem] lg:object-center"
+        />
+      </picture>
+
+      <picture>
+        <source
+          media="(min-width: 22.5rem)"
+          srcSet="/footer/desk-footer-asset-right.jpg"
+          className="lg:height-[44rem] hidden w-1/2 md:absolute md:-bottom-[2rem] md:right-0 md:block md:h-full md:max-w-[42rem] md:object-cover lg:-bottom-[2rem]"
+        />
+        <img
+          src="/footer/desk-footer-asset-right.jpg"
+          alt=""
+          loading="lazy"
+          className="lg:height-[44rem] hidden w-1/2 md:absolute md:-bottom-[2rem] md:right-0 md:block md:h-full md:max-w-[42rem] md:object-cover lg:-bottom-[2rem]"
+        />
+      </picture>
+
       <div className="flex md:hidden">
-        <img
-          src="/footer/mob-footer-asset-left.webp"
-          alt=""
-          className="w-1/2"
-        />
-        <img
-          src="/footer/mob-footer-asset-right.webp"
-          alt=""
-          className="w-1/2"
-        />
+        <picture>
+          <source
+            media="(max-width: 22.5rem)"
+            srcSet="/footer/mob-footer-asset-left.jpg"
+          />
+          <img src="/footer/mob-footer-asset-left.jpg" alt="" loading="lazy" />
+        </picture>
+        <picture>
+          <source
+            media="(max-width: 22.5rem)"
+            srcSet="/footer/mob-footer-asset-right.jpg"
+          />
+          <img src="/footer/mob-footer-asset-right.jpg" alt="" loading="lazy" />
+        </picture>
       </div>
+
       <footer className="max-width relative">
         <div className="bg-grey-200 md:mx-gutter md:mb-gutter md:rounded-2xl md:border md:border-grey-300">
           <div className="mx-auto w-full">
--- packages/tailwind-config/index.ts
+++ packages/tailwind-config/index.ts
@@ -93,6 +93,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      objectPosition: {
+        '-20rem': '-20rem',
+      }
     },
   },
   plugins: [
