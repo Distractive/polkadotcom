29d786f26f156cd17df49910a8f55d69945f385b
Matt Yates <matt.yates52@gmail.com>
2024-07-08 17:52:10 +0100

    fix: modal type

--- apps/web/sanity/selections/blocks/modal.ts
+++ apps/web/sanity/selections/blocks/modal.ts
@@ -1,11 +1,22 @@
 import { q } from "groqd"
 import type { Selection } from "groqd"
 
+type FormType = "newsletter" | "contact"
+
+const formType = q
+  .string()
+  .refine(
+    (value): value is FormType => value === "newsletter" || value === "contact",
+    {
+      message: 'formType must be either "newsletter" or "contact"',
+    }
+  )
+
 export const modalSelection = {
   _key: q.string(),
   heading: q.string(),
   body: q.string(),
   cta: q.string(),
   modalHeading: q.string(),
-  formType: q.union([q.literal("newsletter"), q.literal("contact")]),
+  formType: formType,
 } satisfies Selection
