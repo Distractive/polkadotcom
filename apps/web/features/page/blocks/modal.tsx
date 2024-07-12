cef9fb8603ee11eb395722eebae54aa205418e47
Matt Yates <matt.yates52@gmail.com>
2024-07-12 13:05:48 +0100

    fix: merge

--- apps/web/features/page/blocks/modal.tsx
+++ apps/web/features/page/blocks/modal.tsx
@@ -25,13 +25,8 @@ export function ModalBlock({ modal }: Props) {
     <Dialog>
       <div className="grid-system max-width">
         <div className="col-span-full px-gutter lg:col-span-8 lg:col-start-3">
-<<<<<<< HEAD
           <div className="background-blur flex flex-col gap-card rounded-2xl border border-grey-300 bg-grey-200/80 p-gutter">
             <div className="flex flex-col gap-copy ">
-=======
-          <div className="background-blur flex flex-col gap-card rounded-2xl border border-grey-300 p-gutter">
-            <div className="flex flex-col gap-copy">
->>>>>>> cee92a07c9e507ae3ee2e505a705200725829645
               <Heading variant="h2">{modal.heading}</Heading>
               <p>{modal.body}</p>
             </div>
