48b60b0bdfbe6bfcbcee55eb4bef458242177acc
adrian-at-stink <107425773+adrian-at-stink@users.noreply.github.com>
2024-07-24 11:37:36 +0100

    chore: add new newsletter (#551)

--- apps/web/sanity/lib/lists.ts
+++ apps/web/sanity/lib/lists.ts
@@ -9,4 +9,8 @@ export const HBSPT_LIST = [
     value: "a5ecd657-6aae-4da0-bf08-f3b994919f0b",
   },
   { title: "Contact", value: "a5269d0b-bb6c-4e56-aa9c-a7758958d541" },
+  {
+    title: "Developer Newsletter",
+    value: "d48f3940-0c86-4493-978b-31c5c7047b8e",
+  },
 ]
