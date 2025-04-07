4d2f2f470dc8b2cca02ed6ec319df8113ca85085
ronnie <hello@ronniepence.com>
2025-04-07 17:50:48 -0400

    fix: fixes presentation mode

--- apps/web/app/api/draft/route.ts
+++ apps/web/app/api/draft/route.ts
@@ -1,27 +1,8 @@
-import { client } from '@/sanity/lib/client';
-import { token } from '@/sanity/lib/token';
 
-const clientWithToken = client.withConfig({ token });
+import { defineEnableDraftMode } from "next-sanity/draft-mode";
+import { client } from "@/sanity/lib/client";
+import { token } from "@/sanity/lib/token"
 
-export async function GET(request: Request) {
-  if (process.env.NEXT_PUBLIC_BUILD_TYPE === 'static') {
-    return new Response('Static export mode', { status: 200 });
-  }
-
-  const { draftMode } = await import('next/headers');
-  const { validatePreviewUrl } = await import('@sanity/preview-url-secret');
-  const { redirect } = await import('next/navigation');
-
-  const url = new URL(request.url);
-  const { isValid, redirectTo = '/' } = await validatePreviewUrl(
-    clientWithToken,
-    url.toString(),
-  );
-
-  if (!isValid) {
-    return new Response('Invalid secret', { status: 401 });
-  }
-
-  draftMode().enable();
-  redirect(redirectTo);
-}
+export const { GET } = defineEnableDraftMode({
+  client: client.withConfig({ token }),
+});
