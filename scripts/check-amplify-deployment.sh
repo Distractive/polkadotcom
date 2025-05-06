ed66e5fe6dd4483cfabbc04ddc017370845cb019
Piotr Peter Chodur <peter@opslayer.com>
2025-05-06 11:02:19 +0200

    feat: update script - WIP

--- scripts/check-amplify-deployment.sh
+++ scripts/check-amplify-deployment.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-echo "Test"
+#!/usr/bin/env bash
+set -e
+
+if [[ $# -lt 4 ]]; then
+  echo "Error: Missing parameters"
+  echo "Usage: $0 <aws_region> <amplify_app_id> <branch_name> <commit_id>"
+  exit 1
+fi
+
+AWS_REGION="$1"
+AMPLIFY_APP_ID="$2"
+BRANCH_NAME="$3"
+COMMIT_ID="$4"
+
+echo "Polling Amplify deployment from region $AWS_REGION and app id $AMPLIFY_ADDP_ID for commit $COMMIT_ID on branch $BRANCH_NAME …"
