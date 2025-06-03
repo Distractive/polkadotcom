3c643023dcfa56a0977a60c265e5749b5aa94c6f
thomas-distractive <thomas@distractive.xyz>
2025-06-03 10:03:02 -0400

    feat: removed postbuild, log

--- apps/web/app/api/stats/static-metrics-store.ts
+++ apps/web/app/api/stats/static-metrics-store.ts
@@ -1,18 +1,18 @@
 // Auto-generated metrics from build process
-// Generated on: 2025-05-16T19:10:56.320Z
+// Generated on: 2025-06-03T13:41:09.697Z
 
 export const staticMetricsStore = {
   getActiveValidators: '600',
-  getApprovedReferendums: 891,
+  getApprovedReferendums: 904,
   getAverageMonthlyGovernanceVoters: 16955,
   getPercentDOTSupplyStaked: '54%',
   getPolkadotUptime30d: '100%',
   getTotalDOTStaked: '855816914.7304394',
-  getTotalFeesUSD30d: 695943.6976716486,
+  getTotalFeesUSD30d: 400469.70050591434,
   getTotalNominators: 31706,
-  getTotalReferendums: 1562,
-  getTotalStablecoinsUSD: 72826063.53939262,
-  getTotalStakers: 70640,
+  getTotalReferendums: 1586,
+  getTotalStablecoinsUSD: 47660215.6650005,
+  getTotalStakers: 70251,
   getTreasuryBalanceUSD: 51573300.16,
   getUniqueAccounts: 14022274,
 };
