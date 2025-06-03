c5813c37609fe9c138ee274dd31f952d90e9f9eb
thomas-distractive <thomas@distractive.xyz>
2025-06-03 12:28:10 -0400

    Fix: internal links to posts (#69)
    \removed extra slash that was creating an absolute url

--- apps/web/app/api/stats/static-metrics-store.ts
+++ apps/web/app/api/stats/static-metrics-store.ts
@@ -1,18 +1,18 @@
 // Auto-generated metrics from build process
-// Generated on: 2025-05-16T16:38:26.067Z
+// Generated on: 2025-05-28T14:06:48.449Z
 
 export const staticMetricsStore = {
   getActiveValidators: '600',
-  getApprovedReferendums: 891,
+  getApprovedReferendums: 903,
   getAverageMonthlyGovernanceVoters: 16955,
   getPercentDOTSupplyStaked: '54%',
   getPolkadotUptime30d: '100%',
   getTotalDOTStaked: '855816914.7304394',
-  getTotalFeesUSD30d: 498014.99042580667,
+  getTotalFeesUSD30d: 198489.10274795003,
   getTotalNominators: 31706,
-  getTotalReferendums: 1562,
-  getTotalStablecoinsUSD: 72826063.53939262,
-  getTotalStakers: 70640,
+  getTotalReferendums: 1581,
+  getTotalStablecoinsUSD: 73474953.26061119,
+  getTotalStakers: 70320,
   getTreasuryBalanceUSD: 51573300.16,
   getUniqueAccounts: 14022274,
 };
