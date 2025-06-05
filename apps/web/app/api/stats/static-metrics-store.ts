3d7b122402e898e5c8fc153a537ebd5476aa8c10
ronnie <hello@ronniepence.com>
2025-06-05 08:10:43 -0400

    fix: add handling for when there are no terms - req for static gen

--- apps/web/app/api/stats/static-metrics-store.ts
+++ apps/web/app/api/stats/static-metrics-store.ts
@@ -1,19 +1,18 @@
 // Auto-generated metrics from build process
-
-// Generated on: 2025-05-28T14:06:48.449Z
+// Generated on: 2025-06-05T12:07:51.130Z
 
 export const staticMetricsStore = {
   getActiveValidators: '600',
-  getApprovedReferendums: 903,
-  getAverageMonthlyGovernanceVoters: 16955,
+  getApprovedReferendums: 905,
+  getAverageMonthlyGovernanceVoters: 16265,
   getPercentDOTSupplyStaked: '54%',
   getPolkadotUptime30d: '100%',
-  getTotalDOTStaked: '855816914.7304394',
-  getTotalFeesUSD30d: 198489.10274795003,
-  getTotalNominators: 31706,
-  getTotalReferendums: 1581,
-  getTotalStablecoinsUSD: 73474953.26061119,
-  getTotalStakers: 70320,
-  getTreasuryBalanceUSD: 51573300.16,
-  getUniqueAccounts: 14022274,
+  getTotalDOTStaked: '852516759.372461',
+  getTotalFeesUSD30d: 776016.4493529818,
+  getTotalNominators: 31276,
+  getTotalReferendums: 1586,
+  getTotalStablecoinsUSD: 48031804.77257664,
+  getTotalStakers: 70246,
+  getTreasuryBalanceUSD: 56218803.86,
+  getUniqueAccounts: 14372235,
 };
