509513d13c82ca487fc157fa929cbd54d18f4c72
thomas-distractive <thomas@distractive.xyz>
2025-07-07 08:59:43 -0400

    fix: lint fix

--- apps/web/app/api/stats/parity/metrics/index.ts
+++ apps/web/app/api/stats/parity/metrics/index.ts
@@ -2,6 +2,7 @@ import { getPolkadotUptime30d } from './get-30d-polkadot-uptime';
 import { getActiveValidators } from './get-active-validators';
 import { getApprovedReferendums } from './get-approved-referendums';
 import { getAverageMonthlyGovernanceVoters } from './get-avg-monthly-governance-voters';
+import { getDailyTreasuryBalanceUSD } from './get-daily-treasury-balance-usd';
 import { getTotalNominators } from './get-nominators';
 import { getPercentDOTSupplyStaked } from './get-percent-staked';
 import { getTotalDOTStaked } from './get-total-dot-staked';
@@ -9,7 +10,6 @@ import { getTotalFeesUSD30d } from './get-total-fees-usd-30d';
 import { getTotalReferendums } from './get-total-referendums';
 import { getTotalStablecoinsUSD } from './get-total-stablecoins';
 import { getTotalStakers } from './get-total-stakers';
-import { getDailyTreasuryBalanceUSD } from './get-daily-treasury-balance-usd';
 import { getUniqueAccounts } from './get-unique-accounts';
 
 export const metricFetchers = {
