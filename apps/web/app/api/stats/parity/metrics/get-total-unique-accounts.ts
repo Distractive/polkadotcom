cabf4ad781bc83f089b8099e09b8addab77a7c09
thomas-distractive <thomas@distractive.xyz>
2025-07-18 11:54:35 -0400

    Fix/metric update (#95)
    -updates unique account, dao members, and referenda metrics
    - fixes treasury balance metric bug

--- apps/web/app/api/stats/parity/metrics/get-total-unique-accounts.ts
+++ apps/web/app/api/stats/parity/metrics/get-total-unique-accounts.ts
@@ -2,7 +2,7 @@ import { getMonthlyUniqueAccountsMetrics } from '../endpoints/monthly-unique-acc
 
 import { getLastDayOfPreviousMonth } from '../../utils/get-last-day-previous-month';
 
-export const getUniqueAccounts = async () => {
+export const getTotalUniqueAccounts = async () => {
   const date = getLastDayOfPreviousMonth();
 
   const response = await getMonthlyUniqueAccountsMetrics({
