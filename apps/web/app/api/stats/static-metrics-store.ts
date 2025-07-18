7399948d31a6e1518390f3156629b972a6a9f2b7
thomas-distractive <thomas@distractive.xyz>
2025-07-18 10:50:13 -0400

    Fix/sitemap not updating (#94)
    added sitemap to build process and updated exclusions

--- apps/web/app/api/stats/static-metrics-store.ts
+++ apps/web/app/api/stats/static-metrics-store.ts
@@ -1,18 +1,18 @@
 // Auto-generated metrics from build process
-// Generated on: 2025-07-10T16:39:09.517Z
+// Generated on: 2025-07-18T13:28:14.175Z
 
 export const staticMetricsStore = {
   getActiveValidators: '600',
-  getApprovedReferendums: 931,
+  getApprovedReferendums: 934,
   getAverageMonthlyGovernanceVoters: 15661,
   getPercentDOTSupplyStaked: '53%',
   getPolkadotUptime30d: '100%',
   getTotalDOTStaked: '836614566.3352035',
-  getTotalFeesUSD30d: 0,
+  getTotalFeesUSD30d: 1693.7021311068984,
   getTotalNominators: 31377,
-  getTotalReferendums: 1660,
-  getTotalStablecoinsUSD: 54381575.7418071,
-  getTotalStakers: 70034,
-  getDailyTreasuryBalanceUSD: 56370177.73,
+  getTotalReferendums: 1677,
+  getTotalStablecoinsUSD: 52639861.6342648,
+  getTotalStakers: 69547,
+  getDailyTreasuryBalanceUSD: 66433038.78,
   getUniqueAccounts: 8470124,
 };
