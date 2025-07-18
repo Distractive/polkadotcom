a4ab7eb225e5bc4e269c4a11db872abc24a5ddd8
thomas-distractive <thomas@distractive.xyz>
2025-07-18 11:01:00 -0400

    fix: updated static metric to fix build issue

--- apps/web/app/api/stats/static-metrics-store.ts
+++ apps/web/app/api/stats/static-metrics-store.ts
@@ -1,19 +1,19 @@
 // Auto-generated metrics from build process
-// Generated on: 2025-07-18T13:28:14.175Z
+// Generated on: 2025-07-18T15:00:27.841Z
 
 export const staticMetricsStore = {
   getActiveValidators: '600',
   getApprovedReferendums: 934,
   getAverageMonthlyGovernanceVoters: 15661,
+  getDailyTreasuryBalanceUSD: 66433038.78,
   getDaoMembers: '1.49 million',
   getPercentDOTSupplyStaked: '53%',
   getPolkadotUptime30d: '100%',
   getTotalDOTStaked: '836614566.3352035',
   getTotalFeesUSD30d: 1693.7021311068984,
   getTotalNominators: 31377,
-  getTotalReferendums: 1677,
+  getTotalReferendums: '1,677 referenda',
   getTotalStablecoinsUSD: 52639861.6342648,
   getTotalStakers: 69547,
-  getDailyTreasuryBalanceUSD: 66433038.78,
-  getUniqueAccounts: 8470124,
+  getTotalUniqueAccounts: 8470124,
 };
