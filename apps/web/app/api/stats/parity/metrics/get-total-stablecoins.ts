2ff256e201f8689ac6545fe925763577555b736d
thomas-distractive <thomas@distractive.xyz>
2025-08-20 09:59:57 -0400

    fix: removed polka wallet from wallet test

--- apps/web/app/api/stats/parity/metrics/get-total-stablecoins.ts
+++ apps/web/app/api/stats/parity/metrics/get-total-stablecoins.ts
@@ -7,21 +7,15 @@ export const getTotalStablecoinsUSD = async () => {
     chain: 'polkadot_asset_hub',
   });
 
-  console.log('USDC response: ', usdcResponse);
-
   const usdtResponse = await getUSDTMetrics({
     relay_chain: 'polkadot',
     chain: 'polkadot_asset_hub',
   });
 
-  console.log('USDT response: ', usdtResponse);
-
   if (!usdcResponse[0] || !usdtResponse[0]) {
     return null;
   }
   const total = usdcResponse[0].sum_of_usdc + usdtResponse[0].sum_of_usdt;
-
-  console.log('Stable coins total: ', total);
 
   return total;
 };
