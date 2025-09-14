7470dc1f66e31a228fbb13cb83415a0338fea3b5
thomas-distractive <thomas@distractive.xyz>
2025-09-14 11:04:04 -0400

    chore: updated screenshots

--- apps/web/tests/chain.test.ts
+++ apps/web/tests/chain.test.ts
@@ -192,7 +192,7 @@ test('Chain', async ({ page }) => {
       }),
     ).toBeVisible();
     await expect(section.getByText('Say goodbye to the era of')).toHaveText(
-      'Say goodbye to the era of siloed networks. Polkadot pioneered the standard for inter-blockchain communication, uniting independent rollups via secure messaging with XCM and reliable bridges to external blockchains.',
+      'Say goodbye to the era of siloed networks. Polkadot pioneered the standard for inter-blockchain communication, uniting independent rollups (also known as parachains) via secure messaging with XCM and reliable bridges to external blockchains.',
     );
     await expect(
       section.getByRole('link', { name: 'Explore XCM' }),
