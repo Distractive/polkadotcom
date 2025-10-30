a32ebb241a66cca5a8ec52466c4bb15812d0e295
thomas-distractive <thomas@distractive.xyz>
2025-10-30 14:35:37 -0400

    feat: banner

--- apps/web/features/banner/banner.tsx
+++ apps/web/features/banner/banner.tsx
@@ -64,12 +64,12 @@ export default function Banner({ banner }: BannerProps) {
         <button
           type="button"
           data-testid="banner-close-button"
-          className="group absolute right-[-1rem] top-[-1rem] z-50 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-grey-300 bg-white transition-colors duration-200 hover:bg-black"
+          className="group absolute right-[-1rem] top-[-1rem] z-50 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-grey-300 bg-white dark:bg-black transition-colors duration-200 hover:bg-black dark:hover:bg-grey-900"
           onClick={handleClose}
         >
           <Icon
             variant="close"
-            className="size-6 text-black dark:text-white transition-colors duration-200 group-hover:text-white"
+            className="size-6 text-black dark:text-white  transition-colors duration-200 group-hover:text-white "
           />
         </button>
       </div>
