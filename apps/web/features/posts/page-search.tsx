b32f27b58f379fd5f1f87ab602883d1e7d5c6ab0
thomas-distractive <thomas@distractive.xyz>
2025-10-30 11:45:48 -0400

    fix: changes input bg to grey

--- apps/web/features/posts/page-search.tsx
+++ apps/web/features/posts/page-search.tsx
@@ -19,7 +19,7 @@ function HitComponent({ hit }: { hit: any }) {
   return (
     <Link
       href={`${hit.slug}`}
-      className="text-blue dark:text-black line-clamp-1 h-10 overflow-hidden rounded-lg p-2 leading-roomy hover:bg-grey-300 dark:hover:bg-grey-500"
+      className="text-blue dark:text-white line-clamp-1 h-10 overflow-hidden rounded-lg p-2 leading-roomy hover:bg-grey-300 dark:hover:bg-grey-500"
     >
       {hit.title}
     </Link>
@@ -32,7 +32,7 @@ function NoResults() {
 
   if (query && results && results.hits.length === 0) {
     return (
-      <div className="p-4 text-gray-500 dark:text-black">
+      <div className="p-4 text-gray-500 dark:text-white">
         No results found for &quot;{query}&quot;
       </div>
     );
@@ -49,11 +49,11 @@ function Wrapper() {
       <div
         data-testid="blog-search"
         className={cn(
-          'peer col-span-full flex items-center gap-1 self-stretch bg-grey-100 dark:bg-white p-4 ring-1 ring-inset ring-grey-300 dark:ring-grey-700 focus-within:ring-grey-500 hover:ring-grey-500 transition-all duration-300',
+          'peer col-span-full flex items-center gap-1 self-stretch bg-grey-100 dark:bg-grey-900 p-4 ring-1 ring-inset ring-grey-300 dark:ring-grey-700 focus-within:ring-grey-500 hover:ring-grey-500 transition-all duration-300',
           query ? 'rounded-t-2xl' : 'rounded-2xl',
         )}
       >
-        <Icon variant="magnify" className="size-6 dark:fill-black" />
+        <Icon variant="magnify" className="size-6 dark:fill-white" />
         <SearchBox
           queryHook={queryHook}
           placeholder="Enter your search"
@@ -61,7 +61,7 @@ function Wrapper() {
             root: 'w-full',
             form: 'relative w-full',
             input:
-              'bg-grey-100 dark:bg-white w-full outline-none placeholder:text-grey-500 text-black',
+              'bg-grey-100 dark:bg-grey-900 w-full outline-none placeholder:text-grey-500 text-black dark:text-white',
             submit: 'hidden',
             reset: 'hidden',
           }}
@@ -69,7 +69,7 @@ function Wrapper() {
       </div>
       <div
         className={cn(
-          'transition-[top,max-height] duration-300 absolute w-full flex-col items-start justify-center overflow-auto rounded-b-2xl border border-t-0 border-grey-300 dark:border-grey-700 bg-grey-100 dark:bg-white px-4 py-2 peer-focus-within:border-grey-500 peer-hover:border-grey-500 z-50',
+          'transition-[top,max-height] duration-300 absolute w-full flex-col items-start justify-center overflow-auto rounded-b-2xl border border-t-0 border-grey-300 dark:border-grey-700 bg-grey-100 dark:bg-grey-900 px-4 py-2 peer-focus-within:border-grey-500 peer-hover:border-grey-500 z-50',
           query
             ? 'pointer-events-auto top-14 max-h-64'
             : 'pointer-events-none top-10 h-0',
